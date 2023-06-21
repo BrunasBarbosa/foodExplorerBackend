b4570025a69a2b82ae2cae6d854848bf6c9ba938
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-06-21 17:22:19 -0300

    create user create service

--- src/database/knex/migrations/20230621144047_createUsers.js
+++ src/database/knex/migrations/20230621144047_createUsers.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable('users', table => {
   table.text('name');
   table.text('email');
   table.text('password');
-  table.text('type');
+  table.text('type').default('comum');
 
   table.timestamp('created_at').default(knex.fn.now());
   table.timestamp('updated_at').default(knex.fn.now());
